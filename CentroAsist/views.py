9f69c4dd69233a021406492de79b060e060c5b78
JazminAzul <yessicaangulo.1992@gmail.com>
2016-10-27 16:50:25 -0300

    Nuevos Cambios en vistas

--- CentroAsist/views.py
+++ CentroAsist/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect,get_object_or_404
-from .forms import (loginForm)
-from django.http import HttpResponse
+from CentroAsistencial.forms import ModelForm,ingresar
+from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from CentroAsist.models import Paciente, Profesional, RegistroHC, Adjuntos, PalabrasClave
 
 
 def login(request):
-     nombre = "Bienvendid@ al Centro Asistencial H3"
-    form = LoginForm()
+    nombre = "Bienvendid@ al Centro Asistencial H3"
+    form = ingresar()
     next_url = request.GET.get('next', '/')
     if request.method =="POST":
-        form = LoginForm(request.POST)
+        form = ingresar(request.POST)
         if form.is_valid():
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
@@ -19,19 +19,33 @@ def login(request):
                 login(request, user)
                 return redirect(request.POST.get('next', '/'))
 
+    
+        else: 
+            form= Registrar()
+
+    return render(request, 'login.html', 
+        { 
+        'nombre':nombre, 
+         'form': form, 
+        })
+
+
 def logout(request):
     auth.logout(request)
     # redirect to a succes page
     return HttpResponseRedirect("/account/loggedout/")
 
-def inicio():
+def home(request):
+    return render_to_response('login.html')
+
+#def inicio():
     
     
 
 
-def registroHC():
+#def registroHC():
 
 
 
 
-def Nuevo_Paciente():
+#def Nuevo_Paciente():
