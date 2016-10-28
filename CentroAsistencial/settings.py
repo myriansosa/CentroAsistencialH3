f849e85eea56b5076b0f38394ac7e48db80f9ad6
Belu Arroyo <maribel.gerardway@gmail.com>
2016-10-28 20:28:02 -0300

    cambios en settings y urls

--- CentroAsistencial/settings.py
+++ CentroAsistencial/settings.py
@@ -57,8 +57,6 @@ TEMPLATES = [
     {   
         
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        
-        'DIRS': '../CentroAsistencial/templates/login.html',
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -88,7 +86,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-AR'
 
 TIME_ZONE = 'UTC'
 
@@ -103,3 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- CentroAsistencial/urls.py
+++ CentroAsistencial/urls.py
@@ -21,14 +21,14 @@ Including another URLconf
 """
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns ('',
+urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/','CentroAsist.views.login', name='login'),
+    url(r'^login/','CentroAsist.views.login_view', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name="auth_logout"),
     url( regex=r'^login/$', view=login, kwargs={'template_name': 'login.html'}, name='login' ), url( regex=r'^logout/$', view=logout, kwargs={'next_page': '/'}, name='logout' ), 
 
-)   
+]
